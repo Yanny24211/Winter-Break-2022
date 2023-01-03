76ea811c687c4e7d5364a7d312927e7b00f7bee2
Yanny Patel <47578696+Yanny24211@users.noreply.github.com>
2023-01-03 18:52:06 -0500

    #

---
+++ OP_JavaScript/part2.js
@@ -0,0 +1,16 @@
+function add7(number){
+    return number + 7; 
+}
+
+function multiply(number1, number2){
+    return number1*number2;
+}
+
+function capitalize(string){
+    let result = string.toLowerCase();
+    return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+function lastLetter(string){
+    return string.slice(-1); 
+}
