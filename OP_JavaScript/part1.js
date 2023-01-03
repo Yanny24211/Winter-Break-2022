793485edfa11a7d3ba98bb87c4fa38f0a6cdfc0d
Yanny Patel <47578696+Yanny24211@users.noreply.github.com>
2023-01-03 18:36:08 -0500

    part2 work added and finished

--- OP_JavaScript/part1.js
+++ OP_JavaScript/part1.js
@@ -1,3 +1,19 @@
-console.log("Hello, World!")
-console.log(28+22)
-console.log(1+2+3+4+5+6)
+console.log("Hello, World!");
+console.log(28+22);
+console.log(1+2+3+4+5+6);
+
+let x = (4+6+9)/77;
+console.log(x);
+
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+//Strings
+const string = "Hello";
+console.log(string);
+const string2 = "World!";
+const greeting = `Hello, ${string2}`; 
+console.log(greeting);
